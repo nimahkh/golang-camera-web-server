aee17682ce584d2a0d2f9e8be13d20a0843b6454
Nima Habibkhoda <nima.habibkhoda@ttgint.com>
2020-11-02 14:25:07 +0300

    init

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"image"
+	"image/color"
 	"log"
 	"net/http"
 	"os"
@@ -22,6 +23,7 @@ var (
 var buffer = make(map[int][]byte)
 var frame []byte
 var mutex = &sync.Mutex{}
+const MinimumArea = 3000
 
 func main() {
 
@@ -72,17 +74,64 @@ func main() {
 func getframes() {
 	img := gocv.NewMat()
 	defer img.Close()
+
+	imgDelta := gocv.NewMat()
+	defer imgDelta.Close()
+
+	imgThresh := gocv.NewMat()
+	defer imgThresh.Close()
+
+	mog2 := gocv.NewBackgroundSubtractorMOG2()
+	defer mog2.Close()
+
+	status := "Ready"
+
+	fmt.Printf("Start reading device: %v\n", 0)
+
 	for {
 		if ok := webcam.Read(&img); !ok {
-			fmt.Printf("Device closed\n")
+			fmt.Printf("Device closed: %v\n", 0)
 			return
 		}
 		if img.Empty() {
 			continue
 		}
+
+		status = "Ready"
+		statusColor := color.RGBA{0, 255, 0, 0}
+
+		// first phase of cleaning up image, obtain foreground only
+		mog2.Apply(img, &imgDelta)
+
+		// remaining cleanup of the image to use for finding contours.
+		// first use threshold
+		gocv.Threshold(imgDelta, &imgThresh, 25, 255, gocv.ThresholdBinary)
+
+		// then dilate
+		kernel := gocv.GetStructuringElement(gocv.MorphRect, image.Pt(3, 3))
+		defer kernel.Close()
+		gocv.Dilate(imgThresh, &imgThresh, kernel)
+
+		// now find contours
+		contours := gocv.FindContours(imgThresh, gocv.RetrievalExternal, gocv.ChainApproxSimple)
+		for i, c := range contours {
+			area := gocv.ContourArea(c)
+			if area < MinimumArea {
+				continue
+			}
+
+			status = "Motion detected"
+			statusColor = color.RGBA{255, 255, 0, 0}
+			gocv.DrawContours(&img, contours, i, statusColor, 1)
+
+			rect := gocv.BoundingRect(c)
+			gocv.Rectangle(&img, rect, color.RGBA{70, 100, 255, 0}, 1)
+		}
+
+		gocv.PutText(&img, status, image.Pt(10, 20), gocv.FontHersheyPlain, 1.2, statusColor, 2)
+
 		frame_id++
 		gocv.Resize(img, &img, image.Point{}, float64(0.5), float64(0.5), 0)
 		frame, _ = gocv.IMEncode(".jpg", img)
-
 	}
 }
